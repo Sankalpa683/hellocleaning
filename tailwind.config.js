672cddadaf3ec71db31481dca80b9b4dbfb9bdcc
Sankalpa683 <sank.dahal@gmail.com>
2024-08-16 18:18:51 +0545

    just checking bruh

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,7 @@ module.exports = {
       },
     },
     extend: {
-      boxShadow: {
-        custom: 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;',
-      },
+     
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
